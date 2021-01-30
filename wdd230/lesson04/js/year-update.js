f3f5716ebaea2d26c5beb1e3b0c4c4f911d75858
dgtlman <44388847+dgtlman@users.noreply.github.com>
2021-01-29 22:02:28 -0500

    burger

--- wdd230/lesson04/js/year-update.js
+++ wdd230/lesson04/js/year-update.js
@@ -5,3 +5,19 @@ window.addEventListener('load', (event)=>{
     const cry = document.querySelector('#copyrightyear');
     cry.textContent = new Date().getFullYear();
 })
+
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+/*** Programming Notes **************************************
+  Arrow Functions - es6 syntactically compact alternative to a regular function expression
+  see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+  or https://www.w3schools.com/js/js_arrow_function.asp
+
+  classList property - https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+  */
