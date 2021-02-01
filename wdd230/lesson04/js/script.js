7c94feb694e7dc72d3a230c828e536ab0d1ee7d1
dgtlman <44388847+dgtlman@users.noreply.github.com>
2021-02-01 02:22:26 -0500

    update date

--- wdd230/lesson04/js/script.js
+++ wdd230/lesson04/js/script.js
@@ -6,26 +6,14 @@ window.addEventListener('load', (event)=>{
 function toggleMenu(){
   document.getElementById("primaryNav").classList.toggle("hide");
 }
-const options = {dateStyle: "full", };
-    document.getElementById('current-date').textContent = new Date().toLocaleDateString('en-US', options);
 
-  /*
-    function getAnswer(){
-      var now = new Date();
-      var month = now.getMonth();
-      var dayOfMonth = now.getDate();
-      var dayOfWeek = now.getDay();
-      let a = document.getElementById("answer");
+function getFormattedDate(){
 
-			if(dayOfWeek ==6 || dayOfWeek==0){
-			  a.innerHTML ="Sleep in.";
-      }
-      else if((month == 0 && dayOfMonth ==1) || (month ==11 && dayOfMonth== 25) || (month == 6 && dayOfMonth ==4)) {
-        a.innerHTML ="Sleep in.";
-      }
-      else {
-        a.innerHTML ="Get up!";
-      }
+  let d=new Date()
+  let weekday=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday",
+                  "Friday","Saturday")
+  let monthname=new Array("January","February","March","April","May","June","July","August",
+                  "September","October","November","December")
 
-    }
-  */
+  return weekday[d.getDay()] + ", " + d.getDate() + " " + monthname[d.getMonth()] + " " + d.getFullYear();
+}
