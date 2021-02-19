050729c8c9f2f6bf65345066648ec4ce058d1d23
dgtlman <44388847+dgtlman@users.noreply.github.com>
2021-02-19 02:57:01 -0500

    js update

--- wdd230/lesson07/js/main.js
+++ wdd230/lesson07/js/main.js
@@ -11,22 +11,13 @@ window.addEventListener('load', (event)=>{
     document.getElementById("primaryNav").classList.toggle("hide");
   };
 
+
 // adds current date to the footer as the last update time/date
   const options = {
     dateStyle: "full",
   };
 
   document.getElementById('current-date').textContent = new Date().toLocaleDateString('en-US', options);
-
-
- // uses local storage to count how many times each unique visitor returns to site.  
-var n = localStorage.getItem('on_load_counter'); 
-if (n === null) {
-    n = 0;
-}
-n++;
-localStorage.setItem("on_load_counter", n);
-document.querySelector('#counter').innerHTML = n;
 
 
 // webfont loader
