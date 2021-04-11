691b19d9b79307a7bd12622bc019628fca6103d4
dgtlman <ericgarofalo@mac.com>
2021-04-11 07:11:35 -0400

    hide work completed

--- wdd230/lesson11/js/storm.js
+++
@@ -1,5 +0,0 @@
-//script for storm center. this is the [storm severity] portion of the form
-
-function adjustRating(rating) {
-  document.getElementById("ratingvalue").innerHTML = rating;
-}
--- wdd230/scripts/year-update.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', (event)=>{
-    const update = document.querySelector('#lastupdated');
-    update.textContent = document.lastModified;
-
-    const year = document.querySelector('#copyrightyear');
-    year.textContent = new Date().getFullYear();
-})
